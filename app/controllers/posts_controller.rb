effa8ddc7a8c2079d7f7cd4fa60bcea1f8c34ca8
Ian Valentine <ian@ivalentine.net>
2015-07-08 15:49:31 -0400

    created post model, controller actions, templates, and links

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
 
+  def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -8,6 +12,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
